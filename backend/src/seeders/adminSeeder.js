33d5cc1f2964211d653218a12293f182852549d9
Sumit Kumar Chaudhary <jaiswalsumit1010@gmail.com>
2026-06-28 12:30:00 +0545

    fixed minor issues

--- backend/src/seeders/adminSeeder.js
+++ backend/src/seeders/adminSeeder.js
@@ -1,46 +1,42 @@
 const db = require("../config/db");
+const bcrypt = require("bcrypt");
 
-const createAdmin = () => {
+const createAdmin = async () => {
+  const admin = db
+    .prepare("SELECT * FROM users WHERE email = ?")
+    .get("jaiswalsumit1010@gmail.com");
 
-    const admin = db.prepare(
-        "SELECT * FROM users WHERE email = ?"
-    ).get("admin@gmail.com");
+  if (!admin) {
+    const hashedPassword = await bcrypt.hash("Herald@12345", 10);
 
+    db.prepare(
+      `
+      INSERT INTO users
+      (
+        first_name,
+        last_name,
+        email,
+        phone,
+        password,
+        role,
+        address
+      )
+      VALUES (?,?,?,?,?,?,?)
+    `,
+    ).run(
+      "Sumit",
+      "Chaudhary",
+      "jaiswalsumit1010@gmail.com",
+      "9704181697",
+      hashedPassword,
+      "admin",
+      "Library",
+    );
 
-    if(!admin){
-
-        db.prepare(`
-            INSERT INTO users
-            (
-                first_name,
-                last_name,
-                email,
-                phone,
-                password,
-                role,
-                address
-            )
-            VALUES (?,?,?,?,?,?,?)
-        `).run(
-            "Sumit",
-            "Chaudhary",
-            "jaiswalsumit1010@gmail.com",
-            "9704181697",
-            "Herald@12345",
-            "admin",
-            "Library"
-        );
-
-
-        console.log("Admin created");
-
-    }else{
-
-        console.log("Admin already exists");
-
-    }
-
+    console.log("Admin created");
+  } else {
+    console.log("Admin already exists");
+  }
 };
 
-
-createAdmin();
+module.exports = createAdmin;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const session = require("express-session");
 
 require("./config/initDb");
 
@@ -15,15 +16,25 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use(
+  session({
+    secret: "library-secret-key",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+    },
+  }),
+);
+
 app.use(express.static(path.join(__dirname, "../../frontend")));
 
 app.use("/", pageRoute);
 
 app.use("/api/auth", authRoute);
-app.use("/api/admin", adminRoute);
-app.use("/api/user", userRoute);
-
+app.use("/admin", adminRoute);
+app.use("/user", userRoute);
 
 app.listen(3000, () => {
-    console.log("Server running on port 3000");
+  console.log("Server running on port 3000");
 });
